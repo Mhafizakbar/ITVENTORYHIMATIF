f88ed975565146555c4a979391f18069f87d28a1
AlyaaChaltaTheopania <12350120896@students.uin-suska.ac.id>
2025-06-05 21:47:30 +0700

    Page Barang

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
-
+import Barang from './pages/Barang';
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
           <Route path="/home" element={<HomePage />} />
+          <Route path="/barang" element={<Barang />} />
       </Routes>
     </Router>
   );
